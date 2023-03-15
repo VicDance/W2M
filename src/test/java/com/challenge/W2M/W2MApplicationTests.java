233e0e2d0d943703980ade7408daad306c8589b5
Vicky <mvictoria_29397@hotmail.es>
2023-03-15 21:24:49 +0100

    set up project

---
+++ src/test/java/com/challenge/W2M/W2MApplicationTests.java
@@ -0,0 +1,13 @@
+package com.challenge.W2M;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class W2MApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
